f1dd1de2b030a34d2b70af35432ef03b07a89bed
TylerShimizu <tyler.monkey2@gmail.com>
2024-03-14 18:23:56 -0700

    Still working progress on shopping cart

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google_api
 
 app = Flask(__name__)
 app.static_folder = "static"
-cart = {}
+cart = []
 
 sheets = google_api.main()
 
@@ -27,26 +27,31 @@ def sub_cat(cat):
     else:
         return render_template('sub_cat.html', category=cat, sub_cats=sheets[cat].keys())
 
-@app.route('/survey_Qs/<category>/<sub_category>')
+@app.route('/survey_Qs/<category>/<sub_category>', methods = ["GET", 'POST'])
 def survey_Qs(category, sub_category):
-    return render_template('Survey_Qs.html', Qs=sheets[category][sub_category])
+    global cart
+    if request.method == 'POST':
+        questions = request.form['q']
+        return redirect(url_for('question_option', question = questions))
+    else:
+        return render_template('Survey_Qs.html', Qs=sheets[category][sub_category], cart = cart, category=category, sub_category=sub_category)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-# Add to Cart route
-@app.route('/add_to_cart/<int:product_id>')
-def add_to_cart(product_id):
-    if 'cart' not in session:
-        session['cart'] = {}
+@app.route('/cart')
+def view_cart():
+    global cart
+    return render_template('cart_view.html', cart = cart)
 
-    if product_id not in session['cart']:
-        session['cart'][product_id] = 1
-    else:
-        session['cart'][product_id] += 1
+@app.route('/add_to_cart/<question>')
+def question_option(question):
 
-    return 'Product added to cart!'
+    # Create a method to get the actual question from the string itself (ie. get_question(question)) or we can create a dictionary of all the
+    # questions when we initialize the other stuff and we can just do a simple look up (ie. q = dictionary[question])    
+    global cart
+    return render_template('question.html', q=question)
 
 if __name__ == '__main__':
     app.run(debug=True)
